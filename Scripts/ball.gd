728a68106ea4147a1741dabbe97fd56fdbb0ff35
Liam Tapper <97718694+tliam1@users.noreply.github.com>
2024-10-05 20:26:32 -1000

    finished basic ball script for applying forces and drag as well

--- Scripts/ball.gd
+++ Scripts/ball.gd
@@ -8,6 +8,8 @@ enum AnimationState {
 }
 
 var currentAnimationState := AnimationState.NORMAL
+var storedBounceForce : Vector3 = Vector3.ZERO
+var drag_factor : float = 0.98
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -21,3 +23,14 @@ func _process(delta):
 
 func _integrate_forces(state):
 	var velocity = state.get_linear_velocity()
+	# applying the bounce force
+	if storedBounceForce != Vector3.ZERO:
+		state.set_linear_velocity(velocity + storedBounceForce)
+		storedBounceForce = Vector3.ZERO
+	
+	# Apply drag to slow the ball down
+	velocity *= drag_factor
+	state.set_linear_velocity(velocity)
+
+func apply_bounce_force(force : Vector3):
+	storedBounceForce = force # a direction vector * bounce force via stats.gd
